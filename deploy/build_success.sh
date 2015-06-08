49e2036844d77cb5fff48090cbb4b6636b9e3737
Stephen Bauer <stevedbauer@gmail.com>
2015-06-08 14:25:10 -0700

    adding remote to build_success.sh script

--- deploy/build_success.sh
+++ deploy/build_success.sh
@@ -1,10 +1,12 @@
 echo "Accessing build_success.sh"
 echo "Branch:"
 echo $TRAVIS_BRANCH
+echo "Adding remote:"
+git remote add my https://github.com/melodicodyssey/TravisTest.git
 echo "Fetching index1:"
-git fetch origin index1
+git fetch my index1
 echo "Checking out index1:"
-git checkout remotes/origin/index1
+git checkout remotes/my/index1
 echo "Creating new branch index1:"
 git checkout -b index1
 echo "Displaying list of branches:"
@@ -14,4 +16,4 @@ git log -5
 echo "Merging from $TRAVIS_BRANCH:"
 git merge $TRAVIS_BRANCH
 echo "Pushing:"
-git push origin index1
+git push my index1
