97fa01b0cb0ae92b91bf6fd0a34eab69e1713449
Stephen Bauer <stevedbauer@gmail.com>
2015-06-08 13:44:59 -0700

    committing deploy/build_success.sh

---
+++ deploy/build_success.sh
@@ -0,0 +1,17 @@
+echo "Accessing build_success.sh"
+echo "Branch:"
+echo $TRAVIS_BRANCH
+echo "Fetching index1:"
+git fetch origin index1
+echo "Checking out index1:"
+git checkout remotes/origin/index1
+echo "Creating new branch index1:"
+git checkout -b 2.0.4-test_setup1
+echo "Displaying list of branches:"
+git branch -a
+echo "Displaying commit log:"
+git log -5
+echo "Merging from $TRAVIS_BRANCH:"
+git merge $TRAVIS_BRANCH
+echo "Pushing:"
+git push origin index1
